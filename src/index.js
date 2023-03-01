8f233c0e246f681a5138d79c2e7b07d7c8afcfb7
chibakaZZ <97170332+chibakaZZ@users.noreply.github.com>
2023-03-01 20:46:50 +0800

    up to date

--- src/index.js
+++ src/index.js
@@ -1,35 +1,68 @@
 //* Requirement dependencies
-const express = require("express");
-const database = require("./config/database");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express"); //todo server asaah
+const mysql = require("mysql");
+// const database = require("./config/database");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// require("dotenv").config();
 
+//* Main server application
 
-const server = express();
+const app = express();
 
-server.use(express.json());
-server.use(cors());
+//* Main server configuration
 
-server.post("/user", (req, res, next) => {
+app.use(express.json());
 
+// server.use(express.json());
+// server.use(cors());
 
-    database.query("SELECT name FROM user", (err, result) => {
-        if (err) throw err
+// server.post("/user", (req, res, next) => {
 
-        console.log(req.body.username)
+//     database.query("SELECT name FROM user", (err, result) => {
+//         if (err) throw err
 
-        if (req.body.username === result[0].name) {
-            res.send("OK")
+//         console.log(req.body.username)
 
-        } else {
-            res.send("KO")
-        }
-    });
+//         if (req.body.username === result[0].name) {
+//             res.send("OK")
+
+//         } else {
+//             res.send("KO")
+//         }
+//     });
+// });
+
+//* Data base connection
+
+const database = mysql.createConnection({
+  host: "localhost",
+  password: "pass1234",
+  user: "root",
+  database: "test",
+});
+
+database.connect((err) => {
+  if (err) throw err;
+
+  console.log("Database connected");
+});
+
+app.get("/user", (req, res) => {
+  database.query("SELECT * FROM user;", (err, result) => {
+    if (err) throw err;
+
+    res.json(result);
+  });
 });
 
 //* Server listen on port
-const port = process.env.PORT;
-server.listen(port, () => {
-    console.log("Server listening on " + port);
+
+app.listen(3003, () => {
+  console.log("Server listening on 3003 PORT...");
 });
+
+// const port = process.env.PORT;
+// server.listen(port, () => {
+//     console.log("Server listening on " + port);
+// });
